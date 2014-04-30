634f79b935e0bf0b4888c5f1e4fca289753b05ff
Noah Petherbridge <root@kirsle.net>
2014-04-30 06:42:12 +0000

    Various improvements to pages

--- www/js/offsite.js
+++ www/js/offsite.js
@@ -8,6 +8,9 @@ $(document).ready(function() {
     $("a").each(function() {
         var $a = $(this);
         var href = $a.attr("href");
+		if (href === undefined) {
+			return;
+		}
 
         // Detect offsite links.
         if (href.indexOf("http:") == 0 || href.indexOf("https:") == 0) {
