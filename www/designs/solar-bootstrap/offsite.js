946552ed5a59ee0e7ccffbbe490c2be64838269a
Noah Petherbridge <root@kirsle.net>
2014-10-16 12:18:26 -0700

    Add old web design archives

---
+++ www/designs/solar-bootstrap/offsite.js
@@ -0,0 +1,20 @@
+/*
+ * Hyperlink Editor Script || Copyright 2014 Kirsle
+ *
+ * This script makes all offsite links open in a new window.
+ */
+
+$(document).ready(function() {
+    $("a").each(function() {
+        var $a = $(this);
+        var href = $a.attr("href");
+		if (href === undefined) {
+			return;
+		}
+
+        // Detect offsite links.
+        if (href.indexOf("http:") == 0 || href.indexOf("https:") == 0) {
+            $a.attr("target", "_blank");
+        }
+    });
+});
