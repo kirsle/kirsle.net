3516969c8885b84c9868ae126eb9955b6d7e0200
Noah Petherbridge <root@kirsle.net>
2015-08-11 17:18:25 -0700

    Spaces to tabs

--- www/js/offsite.js
+++ www/js/offsite.js
@@ -5,16 +5,16 @@
  */
 
 $(document).ready(function() {
-    $("a").each(function() {
-        var $a = $(this);
-        var href = $a.attr("href");
+	$("a").each(function() {
+		var $a = $(this);
+		var href = $a.attr("href");
 		if (href === undefined) {
 			return;
 		}
 
-        // Detect offsite links.
-        if (href.indexOf("http:") == 0 || href.indexOf("https:") == 0) {
-            $a.attr("target", "_blank");
-        }
-    });
+		// Detect offsite links.
+		if (href.indexOf("http:") == 0 || href.indexOf("https:") == 0) {
+			$a.attr("target", "_blank");
+		}
+	});
 });
